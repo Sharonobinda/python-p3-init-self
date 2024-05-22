65ae1e9a256939fae212a2d8c2ee8b340b8ed81f
sharonobinda <obindasharon6@gmail.com>
2024-05-22 16:43:15 +0300

    add solution

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed="Mutt", favorite_toy="Any"):
+        self.name = name
+        self.breed = breed
+        self.favorite_toy = favorite_toy
